64359e2b3ba896db09782a7803d5a4c74a1cc509
Dell <Dell@192.168.1.17>
2024-12-20 22:35:54 +0530

    config file

--- src/main/java/utilities/ConfigReader.java
+++ src/main/java/utilities/ConfigReader.java
@@ -13,8 +13,8 @@ public class ConfigReader {
 		
 		try {
 			
-			FileInputStream file = new FileInputStream("C:\\Users\\Dell\\OneDrive\\Documents\\Desktop\\New folder (2)\\Luma_Cucumber\\src\\test\\resource\\prop.properties");
-			// FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resource\\prop.properties");
+			FileInputStream file = new FileInputStream("C:\\Users\\Dell\\OneDrive\\Documents\\Desktop\\New folder (2)\\Luma_Cucumber\\src\\test\\resource\\cucumberProperties\\prop.properties");
+			// FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resource\\cucumberProperties\\prop.properties");
 			
 			prop.load(file);
 			
